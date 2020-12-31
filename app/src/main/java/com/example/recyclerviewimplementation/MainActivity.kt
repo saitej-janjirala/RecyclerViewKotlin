ebb6221bce5f8334781faec0f0b9ac051b1c8914
janji <janjiralasai420@gmail.com>
2020-12-31 17:10:52 +0530

    used staggered view

--- app/src/main/java/com/example/recyclerviewimplementation/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewimplementation/MainActivity.kt
@@ -4,40 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.animation.AnimationUtils
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity(),MyAdapter.onDeleteClickListener {
     //now we will create our own custom list of objects of type MyData class
-    private lateinit var mylist:ArrayList<MyData>
+    private var mylist= mutableListOf(MyData(R.drawable.sasuke,"Train"))
+    private var mylist2= mutableListOf(MyData(R.drawable.sasuke,"Train"))
+
     private lateinit var myAdapter: MyAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mylist= ArrayList()
-        mylist.add(MyData(R.drawable.ic_baseline_person_24,"Person"))
-        mylist.add(MyData(R.drawable.ic_baseline_directions_car_24,"Car"))
-        mylist.add(MyData(R.drawable.ic_baseline_train_24,"Train"))
-        mylist.add(MyData(R.drawable.ic_baseline_directions_bike_24,"Bike"))
-        mylist.add(MyData(R.drawable.ic_baseline_access_time_24,"Time"))
-        mylist.add(MyData(R.drawable.ic_baseline_person_24,"Person"))
-        mylist.add(MyData(R.drawable.ic_baseline_directions_car_24,"Car"))
-        mylist.add(MyData(R.drawable.ic_baseline_train_24,"Train"))
-        mylist.add(MyData(R.drawable.ic_baseline_directions_bike_24,"Bike"))
-        mylist.add(MyData(R.drawable.ic_baseline_access_time_24,"Time"))
-        mylist.add(MyData(R.drawable.ic_baseline_person_24,"Person"))
-        mylist.add(MyData(R.drawable.ic_baseline_directions_car_24,"Car"))
-        mylist.add(MyData(R.drawable.ic_baseline_train_24,"Train"))
-        mylist.add(MyData(R.drawable.ic_baseline_directions_bike_24,"Bike"))
-        mylist.add(MyData(R.drawable.ic_baseline_access_time_24,"Time"))
+        mylist.add(MyData(R.drawable.kakashi,"Bike"))
+        mylist.add(MyData(R.drawable.tanjiro,"Train"))
+        mylist.add(MyData(R.drawable.nezuko,"Bike"))
+        mylist.add(MyData(R.drawable.naruto,"Person"))
+        mylist.add(MyData(R.drawable.nbrasengan,"Car"))
+        mylist.add(MyData(R.drawable.zenitsu,"Person"))
+        mylist.add(MyData(R.drawable.inosuke,"Car"))
+        mylist.add(MyData(R.drawable.giyu,"Train"))
+        mylist.add(MyData(R.drawable.rengoku,"Train"))
+        mylist.add(MyData(R.drawable.muzan,"Bike"))
+        mylist2.addAll(mylist.subList(1,mylist.size))
 //        mylist.shuffle()//kotlins way of shuffling
         //now we need an adapter to attach our data to our recyclerview
         //to do that first we need to create our recyclerview items layout now lets go
         //our example item is ready lets go create our adapter class
         //lets attach our adapter to the recycler view.
+        mylist.shuffle()
         myAdapter= MyAdapter(this,mylist,this)
+        val layoutmanager=StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
         myrecyclerview.adapter=myAdapter
+        myrecyclerview.layoutManager=layoutmanager
         myrecyclerview.startAnimation(AnimationUtils.loadAnimation(this,R.anim.item_animation_fall_down))
     }
 
--- app/src/main/java/com/example/recyclerviewimplementation/MyAdapter.kt
+++ app/src/main/java/com/example/recyclerviewimplementation/MyAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.recyclerviewimplementation
 
+import android.app.ActionBar
 import android.content.Context
 import android.icu.text.Transliterator
 import android.media.Image
@@ -7,15 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MyAdapter(val context: Context, val list:ArrayList<MyData>,val ondeleteClickListener: onDeleteClickListener)
+class MyAdapter(val context: Context, val list:MutableList<MyData>,val ondeleteClickListener: onDeleteClickListener)
     :RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
     inner class MyViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
         val icon:ImageView=itemView.findViewById(R.id.itemicon)
-        val name:TextView=itemView.findViewById(R.id.itemname)
-        val delete:ImageView=itemView.findViewById(R.id.itemdelete)
+//        val name:TextView=itemView.findViewById(R.id.itemname)
+//        val delete:ImageView=itemView.findViewById(R.id.itemdelete)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
         val view=LayoutInflater.from(context).inflate(R.layout.exampleitem,parent,false)
@@ -26,18 +28,18 @@ class MyAdapter(val context: Context, val list:ArrayList<MyData>,val ondeleteCli
 
     override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
         val obj=list[position]
-        holder.name.text=obj.name
+//        holder.name.text=obj.name
         holder.icon.setImageResource(obj.icon)
         holder.itemView.setOnClickListener {
-            holder.name.text="Index in bounds"
-            //we are trying to change the text when we click on the whole item.
+//            holder.name.text="Index in bounds"
+//            //we are trying to change the text when we click on the whole item.
         }
         //now lets try to delete the item when the user tries to click on the delete icon
-        holder.delete.setOnClickListener{
-            if(position!=RecyclerView.NO_POSITION) {
-                ondeleteClickListener.onDeleteclick(obj,position)
-            }
-        }
+//        holder.delete.setOnClickListener{
+//            if(position!=RecyclerView.NO_POSITION) {
+//                ondeleteClickListener.onDeleteclick(obj,position)
+//            }
+//        }
     }
     interface onDeleteClickListener{
         fun onDeleteclick(myData: MyData,position:Int)
